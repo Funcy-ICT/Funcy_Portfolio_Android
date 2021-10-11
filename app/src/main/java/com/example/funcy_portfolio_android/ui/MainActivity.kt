5ea78186369c0cb047e46fa4f02f4638c3c1d93c
Yuhei Yamamoto <b1018260@fun.ac.jp>
2021-10-11 17:17:07 +0900

    作品のリストを追加

--- app/src/main/java/com/example/funcy_portfolio_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.funcy_portfolio_android.R
 import com.example.funcy_portfolio_android.databinding.ActivityMainBinding
 
@@ -18,6 +20,21 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val Worklist = listOf<work>(
+            work(R.drawable.garden_strand, "garden_strand", "ネックレス"),
+            work(R.drawable.gatsby_hat, "gatsby_hat","ハット"),
+            work(R.drawable.stella_sunglasses, "stella_sunglasses","グラス"),
+            work(R.drawable.strut_earrings, "strut_earrings","イヤリング"),
+            work(R.drawable.vagabond_sack, "vagabond_sack","リュックサック"),
+            work(R.drawable.varsity_sovks, "varsity_sovks","ソックス"),
+            work(R.drawable.whitey_belt, "whitey_belt","ベルト"),
+
+            )
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        recyclerView.adapter = CardAdapter(Worklist)
+        recyclerView.layoutManager = GridLayoutManager(this, 2, RecyclerView.VERTICAL, false)
 
 //        setSupportActionBar(binding.toolbar)
 //
