438d61ae8bc54bda902f0f1b0f908e6393890078
Miyata <b1018188.fun@gmail.com>
2022-05-30 16:07:14 +0900

    [WIP] viewModelの追加と既存のパーツの名前変更など

--- app/src/main/java/com/example/funcy_portfolio_android/ui/signup/SignupFragment.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/signup/SignupFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.funcy_portfolio_android.R
 import com.example.funcy_portfolio_android.databinding.FragmentSignupBinding
@@ -13,19 +15,24 @@ import com.example.funcy_portfolio_android.databinding.FragmentSignupBinding
 class SignupFragment : Fragment() {
 
     private lateinit var binding: FragmentSignupBinding
+    private val viewModel by viewModels<SignupViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSignupBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.button2.setOnClickListener {
+        binding.buttonSignup.setOnClickListener {
             findNavController().navigate(R.id.action_SignupFragment_to_MainFragment)
         }
     }
---
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/signup/SignupViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.funcy_portfolio_android.ui.signup
+
+import androidx.lifecycle.ViewModel
+
+class SignupViewModel: ViewModel() {
+}
