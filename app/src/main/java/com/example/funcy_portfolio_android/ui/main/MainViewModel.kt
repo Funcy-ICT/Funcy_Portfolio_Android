93180d0b6d44c07853cb82e77d7abea6f5619910
yuhei-yama <b1018260.funs@gmail.com>
2022-06-07 01:22:22 +0900

    fix data class and fix name

---
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/main/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.funcy_portfolio_android.ui.main
+
+class MainViewModel {
+}
--- app/src/main/java/com/example/funcy_portfolio_android/ui/main/WorkData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.funcy_portfolio_android.ui.main
-
-data class WorkData(
-    val image: String,
-    val main_title: String,
-    val sub_title: String
-)
