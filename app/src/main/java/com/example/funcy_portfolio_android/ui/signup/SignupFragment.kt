f18b630cd2422d8bb0a527c736e56cc0b5829b18
Miyata <b1018188.fun@gmail.com>
2022-06-10 15:38:21 +0900

    [WIP]選択された学年によってコース一覧が変化するようにした

--- app/src/main/java/com/example/funcy_portfolio_android/ui/signup/SignupFragment.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/signup/SignupFragment.kt
@@ -1,12 +1,15 @@
 package com.example.funcy_portfolio_android.ui.signup
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.funcy_portfolio_android.R
 import com.example.funcy_portfolio_android.databinding.FragmentSignupBinding
@@ -35,6 +38,13 @@ class SignupFragment : Fragment() {
         binding.buttonSignup.setOnClickListener {
             findNavController().navigate(R.id.action_SignupFragment_to_MainFragment)
         }
-    }
 
+        viewModel.selectedItem.observe(viewLifecycleOwner, Observer {
+            viewModel.setCourseId()
+        })
+        viewModel.courseId.observe(viewLifecycleOwner, Observer {
+            val courses = resources.getStringArray(it)
+            viewModel.setCourseSource(courses)
+        })
+    }
 }
--- app/src/main/java/com/example/funcy_portfolio_android/ui/signup/SignupViewModel.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/signup/SignupViewModel.kt
@@ -1,6 +1,35 @@
 package com.example.funcy_portfolio_android.ui.signup
 
+import android.app.Application
+import android.util.Log
+import android.widget.AdapterView
+import androidx.databinding.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.funcy_portfolio_android.R
+
 
 class SignupViewModel: ViewModel() {
+    val selectedItem = MutableLiveData<Int>()
+
+    private var _courseId = MutableLiveData<Int>()
+    val courseId: LiveData<Int> = _courseId
+
+    private var _courseResource = MutableLiveData<Array<String>>()
+    val courseResource: LiveData<Array<String>> = _courseResource
+
+    fun setCourseId(){
+        Log.i("こんにちはスピナーです",selectedItem.value.toString())
+        val item = selectedItem.value!!
+        if(item in 1..4){
+            _courseId.value = R.array.array_bachelor_courses
+        }else if(item > 4){
+            _courseId.value = R.array.array_doctor_courses
+        }
+    }
+
+    fun setCourseSource(courses: Array<String>){
+        _courseResource.value = courses
+    }
 }
