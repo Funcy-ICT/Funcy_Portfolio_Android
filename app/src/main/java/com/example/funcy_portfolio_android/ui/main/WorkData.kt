d88c866a09c6dc604c5a2f072700040ea9ae48ca
yuhei-yama <b1018260.funs@gmail.com>
2022-07-11 18:47:06 +0900

    add network setting

---
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/main/WorkData.kt
@@ -0,0 +1,11 @@
+package com.example.funcy_portfolio_android.ui.main
+
+import android.accounts.AuthenticatorDescription
+
+data class WorkData(
+    val workID: String,
+    val title: String,
+    val image: String,
+    val description: String,
+    val icon: String
+)
