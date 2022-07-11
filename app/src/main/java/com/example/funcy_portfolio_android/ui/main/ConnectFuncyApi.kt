e6d19f629099b3ffa421425a6a710ff3d9636411
yuhei-yama <b1018260.funs@gmail.com>
2022-07-11 18:56:01 +0900

    add ConnectFuncyApi.kt

--- app/src/main/java/com/example/funcy_portfolio_android/ui/main/ConnectFuncyApi.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/main/ConnectFuncyApi.kt
@@ -1,13 +1,17 @@
-package com.example.funcy_portfolio_android.ui.main
+
+import com.example.funcy_portfolio_android.ui.main.WorkData
+
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
 
 //FuncyAPIサーバーの作品一覧取得URL
-private const val SERVER_URL = "http://localhost:8080/article/articles"
+private const val SERVER_URL = "http://10.124.58.83:8080/"
 
 //Moshiオブジェクトの作成
 private val moshi = Moshi.Builder()
@@ -22,8 +26,10 @@ private val retrofit = Retrofit.Builder()
 
 //FuncyAPIサーバーとの通信方法を定義
 interface ConnectFuncyApi{
-    @GET("20") // 取得する作品数
-    suspend fun getArticles():List<ArticleData>
+    @GET("works/10") // 取得する作品数
+    suspend fun getWorks(
+        @Header("token") token:String
+    ):List<WorkData>
 }
 
 object FuncyApi{
