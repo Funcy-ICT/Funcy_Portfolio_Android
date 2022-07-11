3eb8853da593d873c67d7b8f1575189c830ecb2f
yuhei-yama <b1018260.funs@gmail.com>
2022-07-11 19:10:20 +0900

    .idea file

--- app/src/main/java/com/example/funcy_portfolio_android/ui/main/WorkData.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/main/WorkData.kt
@@ -3,9 +3,9 @@ package com.example.funcy_portfolio_android.ui.main
 import android.accounts.AuthenticatorDescription
 
 data class WorkData(
-    val workID: String,
+    val work_id: String,
     val title: String,
-    val image: String,
-    val description: String,
-    val icon: String
+    val images: String,
+    val icon: String,
+    // val description: String,
 )
