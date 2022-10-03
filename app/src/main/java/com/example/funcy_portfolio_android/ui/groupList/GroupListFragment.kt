dac907695aefed9f31234f25137512608c380878
Hayatakwt <shodungiga@gmail.com>
2022-10-03 19:03:45 +0900

    wip:FragmentのViewの作成

--- app/src/main/java/com/example/funcy_portfolio_android/ui/groupList/GroupListFragment.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/groupList/GroupListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.funcy_portfolio_android.R
 
 class GroupListFragment : Fragment() {
@@ -13,5 +15,26 @@ class GroupListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_group_list, container, false)
+    }
+
+    private val groupNames : ArrayList<String> = arrayListOf(
+        "Group Name", "Group Name", "Group Name", "Group Name"
+    )
+
+    private val groupIcon : ArrayList<Int> = arrayListOf(
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var rvGroup : RecyclerView = view.findViewById(R.id.rvGroup)
+
+        val rLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context)
+
+        rvGroup.layoutManager = rLayoutManager
+        rvGroup.adapter = GroupListAdapter(groupIcon, groupNames)
+
+        rvGroup.setHasFixedSize(true)
+
     }
 }
