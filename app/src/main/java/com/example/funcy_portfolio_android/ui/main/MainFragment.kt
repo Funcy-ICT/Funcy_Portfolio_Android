03af34fadea412bc2ee1e4306b72631d1091924f
rekaBcey <rekamey73108@gmail.com>
2022-10-03 17:34:33 +0900

    [add]検索可能性設定

--- app/src/main/java/com/example/funcy_portfolio_android/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.example.funcy_portfolio_android.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -27,6 +27,7 @@ class MainFragment : Fragment() {
         return binding.root
 
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -69,4 +70,70 @@ class MainFragment : Fragment() {
         recyclerView.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
     }
 
+
+
+    /*
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        // Menuの設定
+        inflater.inflate(R.menu.search, menu)
+
+        // app:actionViewClass="android.support.v7.widget.SearchView"のItemの取得
+        val menuItem: MenuItem = menu.findItem(R.id.search_menu_search_view)
+        /**
+         * API level 11以上の場合はこっちを使う
+         *
+         * this.searchView = (SearchView)menuItem.getActionView();
+         */
+
+        // ActionViewの取得
+        this.searchView = MenuItemCompat.getActionView(menuItem) as SearchView
+
+        // 虫眼鏡アイコンを最初表示するかの設定
+        this.searchView.setIconifiedByDefault(true)
+
+        // Submitボタンを表示するかどうか
+        this.searchView.setSubmitButtonEnabled(false)
+        if (!this.searchWord.equals("")) {
+            // TextView.setTextみたいなもの
+            this.searchView.setQuery(this.searchWord, false)
+        } else {
+            val queryHint: String =
+                self.getResources().getString(R.string.search_menu_query_hint_text)
+            // placeholderみたいなもの
+            this.searchView.setQueryHint(queryHint)
+        }
+        this.searchView.setOnQueryTextListener(self.onQueryTextListener)
+    }
+
+    private val onQueryTextListener: SearchView.OnQueryTextListener =
+        object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(searchWord: String): Boolean {
+                // SubmitボタンorEnterKeyを押されたら呼び出されるメソッド
+                return self.setSearchWord(searchWord)
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // 入力される度に呼び出される
+                return false
+            }
+        }
+
+    private fun setSearchWord(searchWord: String?): Boolean {
+        val actionBar: ActionBar = (this.activity as ActionBarActivity?).getSupportActionBar()
+        actionBar.setTitle(searchWord)
+        actionBar.setDisplayShowTitleEnabled(true)
+        if (searchWord != null && searchWord != "") {
+            // searchWordがあることを確認
+            searchWord = searchWord
+        }
+        // 虫眼鏡アイコンを隠す
+        this.searchView.setIconified(false)
+        // SearchViewを隠す
+        this.searchView.onActionViewCollapsed()
+        // Focusを外す
+        this.searchView.clearFocus()
+        return false
+    }
+*/
 }
