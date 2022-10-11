a3fc63597ce57fc98c5e226d696b3075a151ad3b
Hayatakwt <shodungiga@gmail.com>
2022-10-11 11:11:06 +0900

    wip:onBindViewHolder作成

--- app/src/main/java/com/example/funcy_portfolio_android/ui/groupList/GroupListAdapter.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/groupList/GroupListAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.funcy_portfolio_android.ui.groupList
 
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,19 +8,25 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.funcy_portfolio_android.R
 
-class GroupListAdapter(private val groupList: List<GroupListData>) : RecyclerView.Adapter<GroupListAdapter.ViewHolder>() {
+class GroupListAdapter(
+    private val groupIcons: ArrayList<Int>,
+    private val groupNames: ArrayList<String>
+) : RecyclerView.Adapter<GroupListAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val groupIcon: ImageView = view.findViewById(R.id.group_image)
         val groupName: TextView = view.findViewById(R.id.group_name)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val layoutView = LayoutInflater.from(viewGroup.context).inflate(R.layout.create_group, viewGroup, false)
+        val layoutView =
+            LayoutInflater.from(viewGroup.context).inflate(R.layout.create_group, viewGroup, false)
         return ViewHolder(layoutView)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val group = groupList[position]
-        viewHolder.image.setImageResource(group.)
+        viewHolder.groupIcon.setImageResource(groupIcons.get(position))
+        viewHolder.groupName.text = groupNames[position]
     }
+
+    override fun getItemCount() = groupNames.size
 }
