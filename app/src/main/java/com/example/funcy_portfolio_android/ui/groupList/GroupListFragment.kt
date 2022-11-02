77380989c8cdadb515bee73608d21da64c0b698b
Hayatakwt <shodungiga@gmail.com>
2022-11-02 14:07:43 +0900

    wip:円形グループアイコン作成

--- app/src/main/java/com/example/funcy_portfolio_android/ui/groupList/GroupListFragment.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/groupList/GroupListFragment.kt
@@ -1,6 +1,5 @@
 package com.example.funcy_portfolio_android.ui.groupList
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,16 +7,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestBuilder
 import com.example.funcy_portfolio_android.R
+import com.example.funcy_portfolio_android.databinding.FragmentGroupListBinding
 
 class GroupListFragment : Fragment() {
+
+    private lateinit var binding: FragmentGroupListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_group_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_group_list, container, false)
+
+        binding = FragmentGroupListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     private val groupNames : ArrayList<String> = arrayListOf(
@@ -32,7 +36,6 @@ class GroupListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         var rvGroup : RecyclerView = view.findViewById(R.id.rvGroup)
-
         val rLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context)
 
         rvGroup.layoutManager = rLayoutManager
