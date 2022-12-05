9f7ee597865453149cf292506bc40b1c97882452
M0710Fa <b1018188.fun@gmail.com>
2022-12-05 17:49:00 +0900

    AuthenticationFragmentを追加

---
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/authentication/AuthenticationFragment.kt
@@ -0,0 +1,28 @@
+package com.example.funcy_portfolio_android.ui.authentication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.funcy_portfolio_android.R
+import com.example.funcy_portfolio_android.databinding.FragmentAuthenticationBinding
+
+class AuthenticationFragment: Fragment() {
+    private lateinit var binding: FragmentAuthenticationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_authentication, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
