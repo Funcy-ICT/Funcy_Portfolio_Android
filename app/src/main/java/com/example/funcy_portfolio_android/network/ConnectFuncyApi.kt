c6654b596ba1bfa11330eb31c77dd4e7221af897
M0710Fa <b1018188.fun@gmail.com>
2023-06-12 16:54:30 +0900

    [fix]URLをprivateに変更

--- app/src/main/java/com/example/funcy_portfolio_android/network/ConnectFuncyApi.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/network/ConnectFuncyApi.kt
@@ -6,7 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val URL = "http://192.168.3.17:9000/"
+private val URL = "http://192.168.3.17:9000/"
 
 private val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
 
