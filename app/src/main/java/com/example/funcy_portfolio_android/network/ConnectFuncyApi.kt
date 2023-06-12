f5d355f44c934f96910c62ee37536859832d8a6d
M0710Fa <b1018188.fun@gmail.com>
2023-06-12 13:39:21 +0900

    [feat]OkhttpLoggerを追加して通信ログを見やすくする

--- app/src/main/java/com/example/funcy_portfolio_android/network/ConnectFuncyApi.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/network/ConnectFuncyApi.kt
@@ -1,10 +1,18 @@
 package com.example.funcy_portfolio_android.network
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val URL = "http://192.168.3.17:9000/"
+
+private val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+private val httpClient = OkHttpClient.Builder()
+    .addInterceptor(logger)
+    .build()
 
 val gson = GsonBuilder()
     .serializeNulls()
@@ -12,6 +20,7 @@ val gson = GsonBuilder()
 
 private val retrofit = Retrofit.Builder()
     .baseUrl(URL)
+    .client(httpClient)
     .addConverterFactory(GsonConverterFactory.create(gson))
     .build()
 
