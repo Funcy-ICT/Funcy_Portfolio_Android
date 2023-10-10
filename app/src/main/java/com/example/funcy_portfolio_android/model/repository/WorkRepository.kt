2eb266e341a9225fe9998f45f708fd78dca8f617
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-10-10 16:40:43 +0900

    :wrench:getWorkDetailをResponseクラスに合わせた

--- app/src/main/java/com/example/funcy_portfolio_android/model/repository/WorkRepository.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/model/repository/WorkRepository.kt
@@ -1,13 +1,13 @@
 package com.example.funcy_portfolio_android.model.repository
 
 import com.example.funcy_portfolio_android.model.data.WorkData
-import com.example.funcy_portfolio_android.network.ApiService
+import com.example.funcy_portfolio_android.network.ConnectFuncyApi
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 import java.io.IOException
 
 class WorkRepository() {
-    private val service = ApiService.service
+    private val service = ConnectFuncyApi.ApiService.service
 
     suspend fun registerWork(work: WorkData): String? {
         val data = work
@@ -26,7 +26,23 @@ class WorkRepository() {
         return res
     }
 
-    suspend fun getWorkDetail(token: String, workId:String) = service.getWorkDetail(token = token, workId =  workId)
+    suspend fun getWorkDetail(token: String, workId: String): WorkData {
+        val response = service.getWorkDetail(token = token, workId = workId)
+        var workData: WorkData? = null
+        if (response.isSuccessful) {
+            workData = response.body()
+        }
+        return workData ?: WorkData(
+            title = "",
+            description = "",
+            images = listOf(),
+            workUrl = "",
+            movieUrl = "",
+            tags = listOf(),
+            group = null,
+            security = 0
+        )
+    }
 
     suspend fun getWork(token: String) = service.getWorks(token = token)
 }
