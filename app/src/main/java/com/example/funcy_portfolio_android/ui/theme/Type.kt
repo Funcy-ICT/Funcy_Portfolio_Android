f2835991c81e0fd20302bdf1bb1d0dd92880ccb4
M0710Fa <b1018188.fun@gmail.com>
2023-10-10 17:43:00 +0900

    [fix]fontFamilyの宣言と，適用を削除

--- app/src/main/java/com/example/funcy_portfolio_android/ui/theme/Type.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/theme/Type.kt
@@ -1,22 +1,9 @@
 package com.example.funcy_portfolio_android.ui.theme
 
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.funcy_portfolio_android.R
-
-/**
- * font: Noto Sans Japanese
- *
- * CAUTION!: Fonts are subject to change
- */
-val notoSansFamily = FontFamily(
-    Font(R.font.notosansjp_variablefont_wght)
-
-)
 
 /**
  * Funcy Typography
@@ -31,69 +18,59 @@ object FuncyTypography {
         fontSize = 20.sp,
         fontWeight = FontWeight.Bold,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val headlineMedium = TextStyle(
         fontSize = 14.sp,
         fontWeight = FontWeight.Bold,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val titleLarge = TextStyle(
         fontSize = 18.sp,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val titleMedium = TextStyle(
         fontSize = 14.sp,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val titleSmall = TextStyle(
         fontSize = 12.sp,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val bodyMedium = TextStyle(
         fontSize = 14.sp,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val bodySmall = TextStyle(
         fontSize = 12.sp,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val labelLarge = TextStyle(
         fontSize = 18.sp,
         fontWeight = FontWeight.Bold,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val labelMedium = TextStyle(
         fontSize = 16.sp,
         fontWeight = FontWeight.Bold,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 
     val labelSmall = TextStyle(
         fontSize = 14.sp,
         fontWeight = FontWeight.Bold,
         fontStyle = FontStyle.Normal,
-        fontFamily = notoSansFamily
     )
 }
