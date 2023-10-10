cc732d84409f6b450be092414ca3fbf1bbf911ab
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-10-10 16:11:40 +0900

    :wrench:data classの命名修正

--- app/src/main/java/com/example/funcy_portfolio_android/model/data/WorkData.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/model/data/WorkData.kt
@@ -1,11 +1,15 @@
 package com.example.funcy_portfolio_android.model.data
 
+import com.google.gson.annotations.SerializedName
+
 data class WorkData(
     val title: String,
     val description: String,
     val images: List<ImageData>,
-    val URL: String,
-    val movie_url: String,
+    @SerializedName("work_url")
+    val workUrl: String,
+    @SerializedName("movie_url")
+    val movieUrl: String,
     val tags: List<TagData>,
     val group: String?,
     val security: Int,
