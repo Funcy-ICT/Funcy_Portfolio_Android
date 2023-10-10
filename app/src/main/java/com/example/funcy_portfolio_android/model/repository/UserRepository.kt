e5f07fe1ea03a714dd36a125588de84e47429a03
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-10-10 16:41:14 +0900

    :wrench:命名変更部分の修正

--- app/src/main/java/com/example/funcy_portfolio_android/model/repository/UserRepository.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/model/repository/UserRepository.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import com.example.funcy_portfolio_android.model.data.AuthData
 import com.example.funcy_portfolio_android.model.data.SignupData
 import com.example.funcy_portfolio_android.model.localDataSource.SharedPref
-import com.example.funcy_portfolio_android.network.ApiService
+import com.example.funcy_portfolio_android.network.ConnectFuncyApi
 
 class UserRepository {
-    private val service = ApiService.service
+    private val service = ConnectFuncyApi.ApiService.service
 
     fun saveUserId(context: Context, key: String, value: String) = SharedPref(context = context).saveSharedPrefString(key, value)
 
