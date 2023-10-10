ad523795e6bff555e0bd28ee3497c79d01e3311f
Hayatakwt <shodungiga@gmail.com>
2023-10-10 16:15:09 +0900

    fix: WorkDataをSwaggerのSchemaと同じものに変更

--- app/src/main/java/com/example/funcy_portfolio_android/model/data/WorkData.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/model/data/WorkData.kt
@@ -3,18 +3,22 @@ package com.example.funcy_portfolio_android.model.data
 data class WorkData(
     val title: String,
     val description: String,
+    val thumbnail: String,
+    val user_icon: String,
+    val user_name: String,
+    val userID: String,
     val images: List<ImageData>,
-    val URL: String,
+    val work_url: String,
     val movie_url: String,
     val tags: List<TagData>,
-    val group: String?,
+    val group: String,
     val security: Int,
 )
 
 data class ImageData(
-    val Image: String
+    val image: String
 )
 
 data class TagData(
-    val Tag: String
+    val tag: String
 )
