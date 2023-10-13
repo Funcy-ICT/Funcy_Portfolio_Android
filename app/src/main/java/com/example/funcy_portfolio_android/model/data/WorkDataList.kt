101f68b77a626bdca79f92fe142ea3a3e9d94ab1
yuhei-yama <b1018260.funs@gmail.com>
2023-10-13 14:20:28 +0900

    [fix] data class WorkDataList

--- app/src/main/java/com/example/funcy_portfolio_android/model/data/WorkDataList.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/model/data/WorkDataList.kt
@@ -1,7 +1,12 @@
 package com.example.funcy_portfolio_android.model.data
 
+/**
+ * 作品一覧画面(main)のデータクラス
+ */
 data class WorkDataList(
-    val workID: Int,
+    val workID: String,
+    val userID: String,
+    val user_name: String,
     val title: String,
     val thumbnail: String,
     val description: String,
