76626764100f64ff2e4807495c302623a2befbdf
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-10-30 17:37:48 +0900

    build.gradle(project-root)をktsに移行

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,19 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven(url = "https://jitpack.io")
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.3.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21"
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    register<Delete>("clean") {
+        delete(rootProject.buildDir)
+    }
 }
