36e351a0a3da393d27867be82db381a3894c0693
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-11-06 17:39:48 +0900

    lazyListへ置き換え

--- app/src/main/java/com/example/funcy_portfolio_android/ui/groupMypage/GroupMypageFragment.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/groupMypage/GroupMypageFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.platform.ComposeView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.funcy_portfolio_android.R
 import com.example.funcy_portfolio_android.databinding.FragmentGroupMypageBinding
 import com.example.funcy_portfolio_android.model.data.WorkDataList
@@ -21,7 +21,8 @@ class GroupMypageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding= DataBindingUtil.inflate(inflater, R.layout.fragment_group_mypage, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_group_mypage, container, false)
 
         return binding.root
     }
@@ -29,22 +30,25 @@ class GroupMypageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val Worklist = listOf<WorkDataList>(
-            WorkDataList(R.drawable.garden_strand, "garden_strand", "ネックレス"),
-            WorkDataList(R.drawable.gatsby_hat, "gatsby_hat","ハット"),
-            WorkDataList(R.drawable.stella_sunglasses, "stella_sunglasses","グラス"),
-            WorkDataList(R.drawable.strut_earrings, "strut_earrings","イヤリング"),
-            WorkDataList(R.drawable.vagabond_sack, "vagabond_sack","リュックサック"),
-            WorkDataList(R.drawable.varsity_socks, "varsity_sovks","ソックス"),
-            WorkDataList(R.drawable.whitey_belt, "whitey_belt","ベルト"),
-            WorkDataList(R.drawable.copper_wire_rack, "whitey_belt","ラック"),
-            WorkDataList(R.drawable.gilt_desk_trio, "whitey_belt","小物入れ"),
-            WorkDataList(R.drawable.shrug_bag, "whitey_belt","バッグ")
-        )
+        view.findViewById<ComposeView>(R.id.lazyColumnView).apply {
+            setContent {
+                MaterialTheme {
+                    val workList = listOf<WorkDataList>(
+                        WorkDataList(R.drawable.garden_strand, "garden_strand", "ネックレス"),
+                        WorkDataList(R.drawable.gatsby_hat, "gatsby_hat", "ハット"),
+                        WorkDataList(R.drawable.stella_sunglasses, "stella_sunglasses", "グラス"),
+                        WorkDataList(R.drawable.strut_earrings, "strut_earrings", "イヤリング"),
+                        WorkDataList(R.drawable.vagabond_sack, "vagabond_sack", "リュックサック"),
+                        WorkDataList(R.drawable.varsity_socks, "varsity_sovks", "ソックス"),
+                        WorkDataList(R.drawable.whitey_belt, "whitey_belt", "ベルト"),
+                        WorkDataList(R.drawable.copper_wire_rack, "whitey_belt", "ラック"),
+                        WorkDataList(R.drawable.gilt_desk_trio, "whitey_belt", "小物入れ"),
+                        WorkDataList(R.drawable.shrug_bag, "whitey_belt", "バッグ")
+                    )
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.rvGroupWorkList)
-
-        recyclerView.adapter = CardAdapterBefore(Worklist)
-        recyclerView.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+                    GroupMyPageList(workList)
+                }
+            }
+        }
     }
 }
