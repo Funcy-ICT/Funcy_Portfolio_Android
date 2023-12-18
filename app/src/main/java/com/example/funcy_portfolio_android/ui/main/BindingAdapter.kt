38c482c5154711b8fd732099dc991913c4767b8c
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-12-18 17:04:20 +0900

    BindingAdapter修正

--- app/src/main/java/com/example/funcy_portfolio_android/ui/main/BindingAdapter.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/main/BindingAdapter.kt
@@ -8,14 +8,14 @@ import coil.load
 import com.example.funcy_portfolio_android.model.data.WorkDataList
 
 @BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView, data: List<WorkDataList>?){
+fun bindRecyclerView(recyclerView: RecyclerView, data: WorkDataList?) {
     val adapter = recyclerView.adapter as CardAdapter
-    adapter.submitList(data)
+    adapter.submitList(data?.works ?: listOf())
 }
 
 @BindingAdapter("imageUrl")
-fun bindImage(imgView: ImageView, imgUrl: String?){
-    imgUrl?.let{
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         imgView.load(imgUri)
     }
