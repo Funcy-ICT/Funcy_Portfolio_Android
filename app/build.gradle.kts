5638ca438c1a4cad1726c0bb5b3f8666b94b627e
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-12-18 16:49:27 +0900

    compil-sdkを34に

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 @Suppress("UnstableApiUsage")
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.funcy_portfolio_android"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
