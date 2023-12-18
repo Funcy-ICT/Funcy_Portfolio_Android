ba494eea67c0f9b3327eb40fa6bf22dc987ed5e1
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-12-18 16:06:33 +0900

    GroupMyPageのリファクタ

--- app/src/main/java/com/example/funcy_portfolio_android/ui/groupMypage/GroupMypageFragment.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/groupMypage/GroupMypageFragment.kt
@@ -8,13 +8,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.funcy_portfolio_android.R
 import com.example.funcy_portfolio_android.databinding.FragmentGroupMypageBinding
-import com.example.funcy_portfolio_android.model.data.WorkDataList
 
 class GroupMypageFragment : Fragment() {
 
     private lateinit var binding: FragmentGroupMypageBinding
+    private val viewModel: GroupMypageViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,20 +34,7 @@ class GroupMypageFragment : Fragment() {
         view.findViewById<ComposeView>(R.id.lazyColumnView).apply {
             setContent {
                 MaterialTheme {
-                    val workList = listOf<WorkDataList>(
-                        WorkDataList(R.drawable.garden_strand, "garden_strand", "ネックレス"),
-                        WorkDataList(R.drawable.gatsby_hat, "gatsby_hat", "ハット"),
-                        WorkDataList(R.drawable.stella_sunglasses, "stella_sunglasses", "グラス"),
-                        WorkDataList(R.drawable.strut_earrings, "strut_earrings", "イヤリング"),
-                        WorkDataList(R.drawable.vagabond_sack, "vagabond_sack", "リュックサック"),
-                        WorkDataList(R.drawable.varsity_socks, "varsity_sovks", "ソックス"),
-                        WorkDataList(R.drawable.whitey_belt, "whitey_belt", "ベルト"),
-                        WorkDataList(R.drawable.copper_wire_rack, "whitey_belt", "ラック"),
-                        WorkDataList(R.drawable.gilt_desk_trio, "whitey_belt", "小物入れ"),
-                        WorkDataList(R.drawable.shrug_bag, "whitey_belt", "バッグ")
-                    )
-
-                    GroupMyPageList(workList)
+                    GroupMyPageScreen(viewModel)
                 }
             }
         }
--- app/src/main/java/com/example/funcy_portfolio_android/ui/groupMypage/GroupMypageScreen.kt
+++ app/src/main/java/com/example/funcy_portfolio_android/ui/groupMypage/GroupMypageScreen.kt
@@ -1,6 +1,5 @@
 package com.example.funcy_portfolio_android.ui.groupMypage
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,19 +15,28 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.funcy_portfolio_android.R
+import coil.compose.AsyncImage
+import com.example.funcy_portfolio_android.model.data.WorkData
 import com.example.funcy_portfolio_android.model.data.WorkDataList
 
 @Composable
-fun GroupMyPageList(workDataList: List<WorkDataList>) {
+fun GroupMyPageScreen(viewModel: GroupMypageViewModel) {
+    val workDataList by remember { mutableStateOf(viewModel.workList) }
+    GroupMyPageList(workDataList = workDataList)
+}
+
+@Composable
+fun GroupMyPageList(workDataList: WorkDataList){
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -37,7 +45,7 @@ fun GroupMyPageList(workDataList: List<WorkDataList>) {
         // 先頭のmargin用
         items(2) {}
 
-        items(workDataList) {
+        items(workDataList.works) {
             GroupMyPageItem(it)
         }
 
@@ -47,7 +55,7 @@ fun GroupMyPageList(workDataList: List<WorkDataList>) {
 }
 
 @Composable
-fun GroupMyPageItem(workDataList: WorkDataList, onClick: () -> Unit = {}) {
+fun GroupMyPageItem(workData: WorkData, onClick: () -> Unit = {}) {
     Box(
         modifier = Modifier
             .height(200.dp)
@@ -55,13 +63,14 @@ fun GroupMyPageItem(workDataList: WorkDataList, onClick: () -> Unit = {}) {
             .clickable { onClick() }
     ) {
         Column {
-            Image(
-                painter = painterResource(id = workDataList.work_id),
-                contentDescription = null,
+            AsyncImage(
+                model = workData.thumbnail,
+                contentDescription = "",
                 modifier = Modifier
                     .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
                     .height(140.dp),
                 contentScale = ContentScale.Crop
+
             )
             Column(
                 modifier = Modifier
@@ -69,12 +78,12 @@ fun GroupMyPageItem(workDataList: WorkDataList, onClick: () -> Unit = {}) {
                     .padding(horizontal = 12.dp, vertical = 8.dp)
             ) {
                 Text(
-                    text = workDataList.title,
+                    text = workData.title,
                     maxLines = 1,
                     fontSize = 14.sp
                 )
                 Text(
-                    text = workDataList.images,
+                    text = workData.title,
                     fontSize = 12.sp,
                     color = Color(0xFF707F89),
                     maxLines = 1,
@@ -88,17 +97,17 @@ fun GroupMyPageItem(workDataList: WorkDataList, onClick: () -> Unit = {}) {
 @Preview(showBackground = true)
 @Composable
 fun PreviewGroupList() {
-    val workDataList = listOf<WorkDataList>(
-        WorkDataList(R.drawable.garden_strand, "garden_strand", "ネックレス"),
-        WorkDataList(R.drawable.gatsby_hat, "gatsby_hat", "ハット"),
-        WorkDataList(R.drawable.stella_sunglasses, "stella_sunglasses", "グラス"),
-        WorkDataList(R.drawable.strut_earrings, "strut_earrings", "イヤリング"),
-        WorkDataList(R.drawable.vagabond_sack, "vagabond_sack", "リュックサック"),
-        WorkDataList(R.drawable.varsity_socks, "varsity_sovks", "ソックス"),
-        WorkDataList(R.drawable.whitey_belt, "whitey_belt", "ベルト"),
-        WorkDataList(R.drawable.copper_wire_rack, "whitey_belt", "ラック"),
-        WorkDataList(R.drawable.gilt_desk_trio, "whitey_belt", "小物入れ"),
-        WorkDataList(R.drawable.shrug_bag, "whitey_belt", "バッグ")
+    val workDataList = WorkDataList(
+        listOf(
+            WorkData(
+                workID = "",
+                userID = "",
+                user_name = "",
+                title = "",
+                thumbnail = "",
+                icon = ""
+            )
+        )
     )
     MaterialTheme {
         GroupMyPageList(workDataList = workDataList)
